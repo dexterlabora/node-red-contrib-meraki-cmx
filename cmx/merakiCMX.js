a90d69371ab1b1ae082fef92d34a0a53ae5e3a82
Cory Guynn <cory@premiereportal.net>
2016-07-26 15:04:08 +0100

    removed debugging messages

--- cmx/merakiCMX.js
+++ cmx/merakiCMX.js
@@ -17,15 +17,11 @@ module.exports = function (RED) {
 
   function merakiCMXsettings(n) {
       RED.nodes.createNode(this,n);
-
-      console.log('merakiCMXsettings n %j',n);
-      console.log('merakiCMXsettings this  %j',this);
-      this.name = n.name;
-      this.port = n.port;
-      this.url = n.url;
-
-      console.log('merakiCMXsettings this UPDATED '+ JSON.stringify(this, null, 2));
-
+      //this.name = n.name;
+      //this.url = n.url;
+      //console.log('merakiCMXsettings n %j',n);
+      //console.log('merakiCMXsettings this  %j',this);
+      //console.log('merakiCMXsettings this '+ JSON.stringify(this, null, 2));
   }
 
   RED.nodes.registerType("meraki-cmx-settings",merakiCMXsettings,{
@@ -39,89 +35,63 @@ module.exports = function (RED) {
   // output node
   function merakiCMX(n) {
     RED.nodes.createNode(this,n);
+    console.log('merakiCMX n '+JSON.stringify(n, null, 3));
+    this.name = n.name;
+    this.url = n.url;
     // Retrieve the config node
     this.settings = RED.nodes.getNode(n.settings);
-    //this.credentials = RED.nodes.getNode(n.credentials);
-    console.log('merakiCMX n %j',n);
-    console.log('merakiCMX this '+ JSON.stringify(this, null, 2));
+
 
     // copy "this" object in case we need it in context of callbacks of other functions.
-    //var node = this;
     var node = this;
+
+    console.log('merakiCMX this '+ JSON.stringify(this, null, 3));
 
     // Start CMX Listener
     cmxServer(node);
-
   };
   RED.nodes.registerType("Meraki CMX",merakiCMX);
 
   function cmxServer(node){
-    //console.log('cmxServer node: '+JSON.stringify(node, null, 2));
-    console.log('cmxServer url: '+node.settings.url);
+    console.log('cmxServer url: '+node.url);
     console.log('cmxServer validator: '+node.settings.credentials.validator);
-    console.log('cmxServer secret: '+node.settings.credentials.secret);
-
-    var express = require('express');
-    var bodyParser = require('body-parser');
+    //console.log('cmxServer node: '+JSON.stringify(node, null, 2));
+    //console.log('cmxServer secret: '+node.settings.credentials.secret);
 
     var msg = {};
 
-    var app = express();
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-    app.get(node.settings.url, function(req, res){
+    RED.httpAdmin.get(node.url, function(req, res){
       console.log("sending validation: "+node.settings.credentials.validator);
-      msg.payload = node.settings.credentials.validator;
+      msg.validator = node.settings.credentials.validator;
       node.send(msg);
-      res.send(msg.payload);
-
+      res.send(msg.validator);
     });
 
-    app.post(node.settings.url, function(req, res){
+    RED.httpAdmin.post(node.url, function(req, res){
         //var jsoned = JSON.parse(req.body.data);
-        console.log('/cmx validating secret');
+        console.log(node.url+' validating secret');
         console.log('cmx req.body: %j',req.body);
+        console.log('cmx req.body: ',req.body);
       try{
           if (req.body.secret == node.settings.credentials.secret) {
-            console.log('/cmx secret verified');
-
+            console.log(node.url+' secret verified');
             msg.payload = req.body;
             node.send(msg);
             res.status('ok');
-
          }else{
-           console.log('invalid secret');
-           msg.payload = 'invalid secret from: '+req.connection.remoteAddress;
-           node.send();
-           res.sendStatus(401); // unauthorized
+            console.log('invalid secret from: '+req.connection.remoteAddress);
+            msg.error = 'invalid secret from: '+req.connection.remoteAddress;
+            node.send(msg);
+            res.sendStatus(401); // unauthorized
          }
        } catch (e) {
          // An error has occured
-         console.log("Error.  Invalid POST from " + req.connection.remoteAddress + ":");
+         console.log("Error.  Invalid POST from " + req.connection.remoteAddress);
          console.log(e);
-         node.send();
+         msg.error = "Invalid POST from " + req.connection.remoteAddress;
+         node.send(msg);
          res.end();
         }
-    });
-
-    // Start CMX Listener
-    console.log('node.settings.connect? '+ node.settings.connected);
-    if(node.settings.connected === true){
-          console.log('server is already running on port' +node.settings.port);
-    }else {
-      node.settings.connected = true;
-      // start server
-      node.server = app.listen(node.settings.port,function(){
-          console.log("Meraki CMX listening on: "+node.settings.port+':'+node.settings.url);
-      });
-    }
-    node.on("close", function() {
-        // Called when the node is shutdown - eg on redeploy.
-        // Allows ports to be closed, connections dropped etc.
-         console.log('closing connection: '+node.settings.port+':'+node.settings.url);
-         node.server.close();
-         node.settings.connected = null;
     });
 
   }
